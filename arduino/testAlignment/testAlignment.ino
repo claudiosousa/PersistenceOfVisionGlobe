9d198d6534badf4462ec7b25a1e4fb38956f81d7
Claudio <claudio.sousa@gmail.com>
2015-03-13 13:04:32 +0100

    testAlignement

--- arduino/testAlignment/testAlignment.ino
+++ arduino/testAlignment/testAlignment.ino
@@ -3,11 +3,11 @@ void setup() {
   setupSrs();
 
   //BGRBGRBG RBGRBGRB GRBGRBGR
-  //00100100 10010010 010010010
+  //00100100 10010010 01001001
 
   for (int i = 0; i < 3; i++) {
     //RRR
-    byte red[] = { 0B00100100, 0B10010010, 0B010010010 };
+    byte red[] =   { 0B00100100, 0B10010010, 0B01001001 };
     writeToSrs(red);
     delay(1000);
     //GGG
@@ -15,7 +15,7 @@ void setup() {
     writeToSrs(green);
     delay(1000);
     //BBB
-    byte blue[] = { 0B10010010, 0B01001001, 0B00100100 };
+    byte blue[] =  { 0B10010010, 0B01001001, 0B00100100 };
     writeToSrs(blue);
     delay(1000);
   }
@@ -25,19 +25,13 @@ void loop() {
  //BGRBGRBG RBGRBGRB GRBGRBGR
  //00101010 00010101 00001010
 
+ 
+// byte all[] = { 255, 255, 255};
+//  writeToSrs(all);
+
   byte rgb[] = { 0B00101010, 0B00010101, 0B00001010};
   writeToSrs(rgb);
-  /*
-  for  (int j = 0; j < 360; j++) {
-   writeToSr(0B10010010);
-   }
-   for  (int j = 0; j < 360; j++) {
-   writeToSr(0B01001001);
-   }
-   for  (int j = 0; j < 360; j++) {
-   writeToSr(0B00100100);
-   }
-   */
+  
 }
 
 
