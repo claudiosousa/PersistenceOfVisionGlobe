dddd406f2d51dbff44711264f0ec41e1a328e8e0
serge <serge@gmail.com>
2015-05-08 12:09:21 +0200

    test PORT: Sucess

--- arduino/sketches/_1284_20mhz_test_port_AD/_1284_20mhz_test_port_AD.ino
+++ arduino/sketches/_1284_20mhz_test_port_AD/_1284_20mhz_test_port_AD.ino
@@ -1,15 +1,15 @@
 
 void setup() {
-    DDRA = 255;
-    DDRD = 255;
+  DDRA = 255;
+  DDRD = 255;
 }
 
 void loop() {
   for(int i = 0;i<=255;i++){
-    PORTA = i;
-    PORTD = 255-i;
+    PORTD =   PORTA = i;
   }
 }
+
 
 
 
