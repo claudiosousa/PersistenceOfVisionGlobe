d1010384fb0eaa531195226aae21f19ebabcb4dc
claudiosousa <claudio.sousa@gmail.com>
2015-08-26 17:12:39 +0200

    bug fixing

--- comWithBox/server/start_prod.bat
+++ comWithBox/server/start_prod.bat
@@ -1,3 +1,3 @@
-netsh wlan set hostednetwork mode=allow ssid=nexus6_ key=povpovpov
+netsh wlan set hostednetwork mode=allow ssid=nexus6 key=povpovpov
 netsh wlan start hostednetwork
 node server/server.js --port=80 --retry
--- comWithBox/sketches/box/motor.ino
+++ comWithBox/sketches/box/motor.ino
@@ -1,20 +1,19 @@
 #include <Servo.h>
-
+//0-2000-700-900-1200
 Servo firstESC;
 void setupMotor(){
   pinMode(13, OUTPUT);
   firstESC.attach(6);
   firstESC.writeMicroseconds(0);
-  delay(100);
+  delay(100);  
   firstESC.writeMicroseconds(700);
 }
 
 void stopMotor(){
   firstESC.writeMicroseconds(700);
-
 }
 void setMotorSpeed(int speed){
-   firstESC.writeMicroseconds(speed);
+   firstESC.writeMicroseconds(min(1600, speed));
 }
 
 void startMotor(){
